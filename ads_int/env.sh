71e5ef2dc1bb4bc5c86b7bc5edf6b7f48f1d29c2
LyleLuo <luowle@foxmail.com>
2022-11-28 17:09:22 +0800

    project init

---
+++ ads_int/env.sh
@@ -0,0 +1,4 @@
+source /home/t716/luowle/software/spack/share/spack/setup-env.sh
+spack load cuda@10.1.243
+spack load gcc@8.4.0
+# export PATH=/opt/nvidia/nsight-systems-cli/2020.3.1/target-linux-x64:$PATH
